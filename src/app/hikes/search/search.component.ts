6aac0d430dbb9884374997f383fff4208a2af7de
tanya-ivanova <ivanova.itanya@gmail.com>
2023-07-08 04:16:35 +0300

    Fixing bugs in Search

--- src/app/hikes/search/search.component.ts
+++ src/app/hikes/search/search.component.ts
@@ -13,33 +13,38 @@ export class SearchComponent implements OnInit {
     posts: Post[] = [];
     isLoading = false;
     isSearchedHikes = true;
-    isInitial: boolean = true;
+    isInitial: boolean = false;
 
     constructor(
         private postService: PostService,
         private router: Router,
         private route: ActivatedRoute
-    ) {}
+    ) { }
 
-    ngOnInit() {        
+    ngOnInit() {
         this.route.queryParams
             .subscribe((queryParams) => {
                 this.searchValue = queryParams['search'];
 
                 this.postService.search(this.searchValue)
-                    .subscribe(posts => {                        
-                        this.isLoading = true;                        
+                    .subscribe(posts => {
                         this.posts = posts;
                         this.isLoading = false;
                     })
+
+                if (this.searchValue === undefined) {
+                    this.isInitial = true;
+                }
             })
     }
 
     onSearch() {
-        if(this.searchValue !== '') {
+        this.isLoading = true;
+        if (this.searchValue !== '' && this.searchValue !== undefined) {
             this.router.navigate(['/search'], { queryParams: { search: this.searchValue } });
         } else {
             this.router.navigate(['/search']);
+            this.isLoading = false;
         }
 
         this.isInitial = false;
